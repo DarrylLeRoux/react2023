e3d277e2d3bcdded7997e11ef0699304b37a6418
Darryl Le Roux <devdarryl@outlook.com>
2023-01-02 17:28:31 +0200

    Added base component layout

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Greeting = () => {
-  return <h1>My First Component</h1>;
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return <h2>Image Placeholder</h2>;
+};
+
+const Title = () => {
+  return <h2>Book Title</h2>;
+};
+
+const Author = () => {
+  return <h4>Author</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Greeting />);
-
-export default Greeting;
+root.render(<BookList />);
