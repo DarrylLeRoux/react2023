2057dd2f213da05f9a2d9ac824b67781af28d692
Darryl Le Roux <devdarryl@outlook.com>
2023-01-03 21:52:16 +0200

    Added two objects to use as props for the two book components

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -4,22 +4,37 @@ import ReactDOM from 'react-dom/client';
 // styles
 import './index.css';
 
-const author = 'James Clear';
-const title = 'Atomic Habits';
-const img = './images/book-1.webp';
+const firstBook = {
+  author: 'Jordan Moore',
+  title: 'Interesting Facts For Curious Minds',
+  img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/71m+Qtq+HrL._AC_UL600_SR600,400_.jpg',
+};
+const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
       <Book />
       <Book />
     </section>
   );
 };
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
