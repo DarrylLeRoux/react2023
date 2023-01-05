a5a722b84cdaaec3aa232e595cadc637044d00ef
Darryl Le Roux <devdarryl@outlook.com>
2023-01-05 13:27:45 +0200

    Added the key for .map()

--- fundamentals/src/main.jsx
+++ fundamentals/src/main.jsx
@@ -9,11 +9,13 @@ const books = [
     author: 'Jordan Moore',
     title: 'Interesting Facts For Curious Minds',
     img: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T2/images/I/71m+Qtq+HrL._AC_UL600_SR600,400_.jpg',
+    id: 1,
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
   },
 ];
 
@@ -21,8 +23,8 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+        const { img, title, author, id } = book;
+        return <Book img={img} title={title} author={author} key={id} />;
       })}
     </section>
   );
