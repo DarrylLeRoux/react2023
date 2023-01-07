3a60a9fdcb7a6fff6675770faa71aeeaba10f1e9
Darryl Le Roux <devdarryl@outlook.com>
2023-01-07 21:25:34 +0200

    Added a new function to display the book that corresponds to the click event. However, it is passed through with a callback and not as a function itself - else it would immediately call it

--- fundamentals/src/main.jsx
+++ fundamentals/src/main.jsx
@@ -26,23 +26,40 @@ const BookList = () => {
   const displayValue = () => {
     console.log(someValue);
   };
+  // A. Find a book that matches the id of the book when clicking. First setup the function in the parent component
+  const getBookID = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+
   return (
     <section className="booklist">
       {books.map((book) => {
         // 3. use the displayValue as a prop and pass the function in
-        return <Book {...book} key={book.id} displayValue={displayValue} />;
+        return (
+          <Book
+            {...book}
+            key={book.id}
+            displayValue={displayValue}
+            // B. Pass the function as a prop
+            getBookID={getBookID}
+          />
+        );
       })}
     </section>
   );
 };
 
-const Book = ({ img, author, title, displayValue }) => {
+// C. Destructure the function getBookID, as well as the id needed in order to reference when the button is clicked
+const Book = ({ img, author, title, displayValue, getBookID, id }) => {
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       {/* 4. Book now uses the prop of display value */}
       <button onClick={displayValue}>Display Title</button>
+      {/* D. onClick is setup with a callback function as we need to invoke it with the id passed to the getBookID function. However, if we immediately call it, it will not run as we expect */}
+      <button onClick={() => getBookID(id)}>Display Book ID</button>
       <h4 className="author">{author}</h4>
       {/* {children} */}
     </article>
