2133620bb68de8ff83d5ce2119695382ab1a1842
Darryl Le Roux <devdarryl@outlook.com>
2023-01-08 22:15:24 +0200

    Added dynamic info for tours

--- backroads-app/src/components/data.js
+++ backroads-app/src/components/data.js
@@ -1,3 +1,9 @@
+// Import the images
+import tour1 from '../images/tour-1.jpeg';
+import tour2 from '../images/tour-2.jpeg';
+import tour3 from '../images/tour-3.jpeg';
+import tour4 from '../images/tour-4.jpeg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -29,5 +35,48 @@ export const services = [
     icon: 'fas fa-socks fa-fw',
     title: 'Amazing Comfort',
     text: 'Rest easy inside the comfortable rooms with numerous options available',
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2023',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'China',
+    duration: '6',
+    price: '$2100',
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'October 11th, 2023',
+    title: 'Best of Java',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Indonesia',
+    duration: '11',
+    price: '$1400',
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'September 15th, 2023',
+    title: 'Explore Hong Kong',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Hong Kong',
+    duration: '8',
+    price: '$5000',
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'December 5th 2023',
+    title: 'Kenya Highlights',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Kenya',
+    duration: '20',
+    price: '$3300',
   },
 ];
