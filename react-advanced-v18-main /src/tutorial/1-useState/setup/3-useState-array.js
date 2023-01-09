e3c424a1e7597d9b091831bacc753c1839036762
Darryl Le Roux <devdarryl@outlook.com>
2023-01-09 22:36:20 +0200

    Added remove items using state

--- react-advanced-v18-main /src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced-v18-main /src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        const removeItem = (id) => {
+          let newPeople = people.filter((person) => person.id !== id);
+          setPeople(newPeople);
+        };
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button className='btn' onClick={() => removeItem(id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button
+        className='btn'
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        Clear Items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
