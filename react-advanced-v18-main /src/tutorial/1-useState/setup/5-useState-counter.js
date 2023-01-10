d7b6adf536eb7e0f2c4ac09f8a2bc818b61aa4d8
Darryl Le Roux <devdarryl@outlook.com>
2023-01-10 19:59:30 +0200

    Added counter

--- react-advanced-v18-main /src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-v18-main /src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,27 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleSubtract = () => {
+    setCount((prev) => prev - 1);
+  };
+  const handleAddition = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  return (
+    <div className='container'>
+      <h4>Counter</h4>
+      <p>{count}</p>
+      <button className='btn' onClick={handleSubtract}>
+        -
+      </button>{' '}
+      <button className='btn' onClick={handleAddition}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default UseStateCounter;
