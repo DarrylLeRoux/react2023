17bfdecc15128aa2d215d40032fe89ae28c47687
Darryl Le Roux <devdarryl@outlook.com>
2023-01-10 20:31:59 +0200

    Added complex counter, and project folder

--- react-advanced-v18-main /src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-v18-main /src/tutorial/1-useState/setup/5-useState-counter.js
@@ -4,23 +4,50 @@ const UseStateCounter = () => {
   const [count, setCount] = useState(0);
 
   const handleSubtract = () => {
-    setCount((prev) => prev - 1);
+    if (count > 0) {
+      setCount(count - 1);
+    }
   };
+  const handleReset = () => {
+    setCount(0);
+  };
+
   const handleAddition = () => {
-    setCount((prev) => prev + 1);
+    setCount(count + 1);
+  };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setCount((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
   };
 
   return (
-    <div className='container'>
-      <h4>Counter</h4>
-      <p>{count}</p>
-      <button className='btn' onClick={handleSubtract}>
-        -
-      </button>{' '}
-      <button className='btn' onClick={handleAddition}>
-        +
-      </button>
-    </div>
+    <>
+      <section>
+        <h4>Counter</h4>
+        <h1>{count}</h1>
+        <button className='btn' onClick={handleSubtract}>
+          -
+        </button>
+        <button onClick={handleReset} className='btn'>
+          Reset
+        </button>
+        <button className='btn' onClick={handleAddition}>
+          +
+        </button>
+      </section>
+      <br />
+      <section>
+        <h4>Complex Counter</h4>
+        <h1>{count}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          Increase Later
+        </button>
+      </section>
+    </>
   );
 };
 
